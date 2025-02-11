c97b7239c8cc073cde8ae88382cb20119a6bdee8
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2025-02-11 21:23:02 +0200

    ADD func for change task status

--- frontend/src/components/ToDo-card.jsx
+++ frontend/src/components/ToDo-card.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToDo = ({ color, taskText }) => {
+	const [statusColor, setStatusColor] = useState('var(--status-color1)')
+
+	const handleStatusClick = () => {
+		setStatusColor(prevColor =>
+			prevColor === 'var(--status-color1)'
+				? 'var(--status-color2)'
+				: 'var(--status-color1)'
+		)
+	}
+
 	return (
 		<div className='col-lg-4 col-md-6 col-xs-12' id='task2'>
 			<div className='content' id={color}>
@@ -10,7 +20,8 @@ const ToDo = ({ color, taskText }) => {
 				</div>
 				<div
 					className='status'
-					style={{ backgroundColor: 'var(--status-color1)' }}
+					style={{ backgroundColor: statusColor }}
+					onClick={handleStatusClick}
 				></div>
 			</div>
 		</div>
